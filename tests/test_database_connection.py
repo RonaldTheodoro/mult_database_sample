7bea634e490dc72b9060cc24513fcd1856089928
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-12-26 19:09:14 -0300

    Add session tests

--- tests/test_database_connection.py
+++ tests/test_database_connection.py
@@ -1,4 +1,5 @@
 from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm.session import Session
 
 
 def test_create_engine_mysql(database_connections):
@@ -13,3 +14,17 @@ def test_create_engine_postgres(database_connections):
     assert isinstance(engine, Engine)
     assert engine.driver == 'psycopg2'
     assert engine.name == 'postgresql'
+
+
+def test_create_session_mysql(database_connections):
+    session = database_connections.create_session('app01_dev')
+    assert isinstance(session, Session)
+    assert session.bind.driver == 'pymysql'
+    assert session.bind.name == 'mysql'
+
+
+def test_create_session_postgres(database_connections):
+    session = database_connections.create_session('app02')
+    assert isinstance(session, Session)
+    assert session.bind.driver == 'psycopg2'
+    assert session.bind.name == 'postgresql'
