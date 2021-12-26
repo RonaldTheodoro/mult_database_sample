cdf285cbb315b11fba2f02be064e02f687b31ea6
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-12-26 20:03:54 -0300

    Implementa get_session

--- controllers/get_controllers.py
+++ controllers/get_controllers.py
@@ -1,12 +1,20 @@
 from controllers.app01_controller import App01Controller
 from controllers.app02_controller import App02Controller
-
+from settings.db.database_connection_factory import connections
 
 class Controllers:
     controllers = {
         'app01': App01Controller,
         'app02': App02Controller,
     }
+
+    def get_session(self, app, stage=None):
+        database = app if stage is None else f'{app}_{stage}'
+        session = connections(database)
+
+        controller = self.get_controller(app)
+        instance = controller(session, database, stage)
+        return instance
 
     def get_controller(self, app):
         if not self.has_controller_for_app(app):
